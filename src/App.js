acc1e22a7b1176aba67016d5ba6469320487b38d
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-07 19:03:05 +0530

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 const App = () => {
     return (
-        <View />
+        <Provider store={createStore(reducers)}>
+            <View />
+        </Provider>
     );
 };
 
