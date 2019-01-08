16095d72e516cf85240f525285492469aacccc1a
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-08 20:56:26 +0530

    send action on library click

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
@@ -7,11 +7,18 @@ import * as actions from '../actions';
 class ListItem extends Component {
     render() {
         const { titleStyle } = styles;
+        const { id, title } = this.props.library;
 
         return (
-            <CardSection>
-                <Text style={titleStyle}>{this.props.library.title}</Text>
-            </CardSection>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>{title}</Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
