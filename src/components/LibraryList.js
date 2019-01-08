ec5f0b14391ae54abac76a9b0f9f6d4878a11dd7
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-08 18:58:33 +0530

    list item component (for showing a single library)

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
     renderItem(library) {
-
+        return <ListItem library={library} />;
     }
 
     render() {
