c34d8ad434bae08616bdf07dfb7d7676491a99a2
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-08 22:18:08 +0530

    add animation

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /** @format */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, UIManager } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+UIManager.setLayoutAnimationEnabledExperimental(true);
 AppRegistry.registerComponent(appName, () => App);
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
+
     renderDescription() {
         const { library, expanded } = this.props;
 
         if (expanded) {
             return (
-                <Text>{library.description}</Text>
+                <CardSection>
+                    <Text style={{ flex: 1 }}>{library.description}</Text>
+                </CardSection>
             );
         }
     }
